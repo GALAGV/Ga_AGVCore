1ee8d90ba4c67fde218d45152b451ca3c027763c
Post520 <781937858@qq.com>
2018-10-30 15:27:34 +0800

    0.0

---
+++ Ga_AGV.Core/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ga_AGV.Core.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Main()
+        {
+            return View();
+        }
+    }
+}

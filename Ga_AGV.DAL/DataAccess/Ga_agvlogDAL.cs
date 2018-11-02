28bf4ac0af91b18a541a514d0b104ca98da314c1
Post781937858 <781937858@qq.com>
2018-11-02 10:27:04 +0800

    0

---
+++ Ga_AGV.DAL/DataAccess/Ga_agvlogDAL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ga_AGV.DAL.DataAccess
+{
+    public class Ga_agvlogDAL
+    {
+
+    }
+}

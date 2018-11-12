dce4ef6f346ff99387d68f478b94aad664cb0869
Post781937858 <781937858@qq.com>
2018-11-12 16:14:30 +0800

    登录MD5加密

--- Ga_AGV.Core/API/agvloginController.cs
+++ Ga_AGV.Core/API/agvloginController.cs
@@ -30,8 +30,8 @@ namespace Ga_AGV.Core.API
         public JsonResult LoginUser([FromBody] Ga_user User)
         {
             var Context = HttpContext.Current;
-            //Ga_user user = Ga_AgvloginBLL.UserLogin(Md5.Encrypt(User.userName), Md5.Encrypt(User.userPassword));
-            Ga_user user = Ga_AgvloginBLL.UserLogin(User.userName,User.userPassword);
+            Ga_user user = Ga_AgvloginBLL.UserLogin(Md5.Encrypt(User.userName), Md5.Encrypt(User.userPassword));
+            //Ga_user user = Ga_AgvloginBLL.UserLogin(User.userName,User.userPassword);
             if (user != null)
             {
                 Context.Session["User"] = user;
